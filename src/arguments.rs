1697afad08a85ca6e826cde8cd85f79db55a263d
onlyd <onlyd@pop-os.home.lla.com>
2026-09-25 22:29:06 -0500

    agregado: un nuevo metodo para verificar la existencia de argumentos, std::env::args

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,4 +1,5 @@
-pub fn get(txt: String) -> bool {
+/// Devuelve True o False si existe o no un argumento
+pub fn exist(txt: String) -> bool {
     let args: Vec<String> = std::env::args().collect();
 
     return match args.iter().position(|arg| arg == &txt) {
@@ -6,3 +7,14 @@ pub fn get(txt: String) -> bool {
         None => false,
     };
 }
+/// Devuelve el valor del argumento
+pub fn get(txt: String) -> Option<String> {
+    let args: Vec<String> = std::env::args().collect();
+
+   let r =   args.iter().position(|arg| arg == &txt)
+ .and_then(|index| args.get(index+1));
+match r { 
+Some( e) => Some(e.clone()),
+None=> None
+}
+ }
